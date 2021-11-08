044258651cb19dce2936b439ee053ca68642eb17
mzarhou <mzarhou.cc@gmail.com>
2021-11-08 13:07:30 +0100

    fix: lstclear

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,23 +6,28 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:40:00 by mzarhou           #+#    #+#             */
-/*   Updated: 2021/11/06 14:20:10 by mzarhou          ###   ########.fr       */
+/*   Updated: 2021/11/08 11:19:51 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	ft_lstclear(t_list **lst_ptr, void (*del)(void*))
 {
 	t_list	*list;
+	t_list	*next_el;
 
 	if (!lst_ptr)
 		return ;
 	list = *lst_ptr;
 	while (list)
 	{
-		ft_lstdelone(list, del);
-		list = list->next;
+		next_el = list->next;
+		if (del)
+			(*del)(list->content);
+		free(list);
+		list = next_el;
 	}
 	*lst_ptr = 0;
 }
--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:29:43 by mzarhou           #+#    #+#             */
-/*   Updated: 2021/11/06 14:20:16 by mzarhou          ###   ########.fr       */
+/*   Updated: 2021/11/08 12:09:40 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (del)
-		(*del)(lst->content);
-	free(lst);
+	if (lst)
+	{
+		if (del)
+			(*del)(lst->content);
+		free(lst);
+	}
 }
