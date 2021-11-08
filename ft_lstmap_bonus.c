3600d9a1f2487fe6c739d2a622124f2216af0565
mzarhou <mzarhou.cc@gmail.com>
2021-11-08 13:08:32 +0100

    fix: lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,63 +6,28 @@
 /*   By: mzarhou <mzarhou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:52:29 by mzarhou           #+#    #+#             */
-/*   Updated: 2021/11/06 14:20:36 by mzarhou          ###   ########.fr       */
+/*   Updated: 2021/11/08 12:36:15 by mzarhou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static void	ft_remove_el(
-	t_list **root, t_list *del_element, void (*del)(void *))
-{
-	if (!root)
-		return ;
-	if (*root == del_element)
-	{
-		*root = (*root)->next;
-		ft_lstdelone(del_element, del);
-		return ;
-	}
-	while (*root)
-	{
-		if ((*root)->next == del_element)
-		{
-			(*root)->next = (*root)->next->next;
-			ft_lstdelone(del_element, del);
-			break ;
-		}
-		*root = (*root)->next;
-	}
-}
-
-static void	ft_add_el(t_list **new_list, void *content)
-{
-	if (!*new_list)
-		*new_list = ft_lstnew(content);
-	else
-		ft_lstadd_back(new_list, ft_lstnew(content));
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*root;
 	t_list	*new_list;
-	void	*content;
-	t_list	*del_el;
+	t_list	*new_el;
 
 	new_list = 0;
 	while (lst)
 	{
-		content = (*f)(lst->content);
-		if (!content)
+		new_el = ft_lstnew((*f)(lst->content));
+		if (!new_el)
 		{
-			del_el = lst;
-			lst = lst->next;
-			ft_remove_el(&root, del_el, del);
-			continue ;
+			ft_lstclear(&new_list, del);
+			return (0);
 		}
-		ft_add_el(&new_list, content);
+		ft_lstadd_back(&new_list, new_el);
 		lst = lst->next;
 	}
 	return (new_list);
